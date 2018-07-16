68826e49cc5f255b1211a7ccc57e40d56afc6018
mklepper <klepper.max@gmail.com>
2018-07-16 16:05:55 +0200

    Update
    Update change pw

--- change_password.php
+++ change_password.php
@@ -2,8 +2,10 @@
 require_once("inc/header.php");
 
 $page = "Change your Password";
-if ($_POST) {
-    if ((isset($_POST['pseudo']) && isset($_POST['email']))) {
+if ($_POST) 
+{
+    if (isset($_POST['pseudo']) && isset($_POST['email'])) 
+    {
 
         $req = "SELECT * FROM user WHERE pseudo = :pseudo AND email=:email ";
 
@@ -11,54 +13,36 @@ if ($_POST) {
         $result1->bindValue(":pseudo", $_POST['pseudo'], PDO::PARAM_STR);
         $result1->bindValue(":email", $_POST['email'], PDO::PARAM_STR);
 
-        if ($result1->execute()) {
+        if ($result1->execute()) 
+        {
             $user = $result1->fetch();
+            debug($user);
 
-            $result = $pdo->prepare("UPDATE user SET pseudo=:pseudo,  pwd=:pwd, id_user=:id_user, firstname=:firstname ,lastname=:lastname, email=:email, address=:address, zip_code=:zc, city=:city, gender=:gender WHERE id_user = :id_user");
+            $result = $pdo->prepare("UPDATE user SET pseudo=:pseudo,  pwd=:pwd, email=:email, address=:address, zip_code=:zc, city=:city, gender=:gender WHERE id_user = $user[id_user]");
 
             $hashed_pwd = password_hash($_POST['pwd'], PASSWORD_BCRYPT);
 
-            $result->bindValue(':id_user', $_POST['id_user'], PDO::PARAM_STR);
             $result->bindValue(':pwd', $hashed_pwd, PDO::PARAM_STR);
             $result->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
-            $result->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
-            $result->bindValue(':lastname', $_POST['lastname'], PDO::PARAM_STR);
             $result->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
-            $result->bindValue(':address', $_POST['address'], PDO::PARAM_STR);
-            $result->bindValue(':zc', $_POST['zc'], PDO::PARAM_STR);
-            $result->bindValue(':gender', $_POST['gender'], PDO::PARAM_STR);
-            $result->bindValue(':city', $_POST['city'], PDO::PARAM_STR);
 
             if ($result->execute()) {
-                echo 'Password changed with sucess';
+            echo 'Password changed with sucess';
              //header('location:login.php');
             }
-        } else
+            }else{
+                echo 'Something got wrong....';
 
-            echo 'Something got wrong....';
+        }
     }
 }
 
-// debug($_POST);
-
-$firstname = (isset($user['firstname'])) ? $user['firstname'] : '';
-$lastname = (isset($user['lastname'])) ? $user['lastname'] : '';
-$address = (isset($user['address'])) ? $user['address'] : '';
-$zip_code = (isset($user['zc'])) ? $user['zc'] : '';
-$city = (isset($user['city'])) ? $user['city'] : '';
-$gender = (isset($user['gender'])) ? $user['gender'] : '';
-$pwd = (isset($user['pwd'])) ? $user['pwd'] : '';
-$id_user = (isset($user['id_user'])) ? $user['id_user'] : '';
+debug($_POST);
 
 ?>
 <h1><?= $page ?></h1>
 <?= $msg_error ?>
 <form method="post">
-
-<!-- Always need to add a 'hidden' id_user input to change a password based on pseudo verification -->
-
-    <input type='hidden' name="id_user" value="<?= $id_user ?>">
-<!-- ____________________________________________________________________________________________________ -->
 
     <div class="form-group">
         <input type="text" class="form-control" name= "pseudo" placeholder = "your pseudo...">
@@ -71,13 +55,6 @@ $id_user = (isset($user['id_user'])) ? $user['id_user'] : '';
     <div class="form-group">
         <input type="text" class="form-control" name = "pwd" placeholder= "your password..." >
     </div>
-
-    <input type='hidden' name="firstname" value="<?= $firstname ?>">
-    <input type='hidden' name="lastname" value="<?= $lastname ?>">
-    <input type='hidden' name="city" value="<?= $city ?>">
-    <input type='hidden' name="zc" value="<?= $zip_code ?>">
-    <input type='hidden' name="address" value="<?= $address ?>">
-    <input type='hidden' name="gender" value="<?= $gender ?>">
 
     <input type="submit" value="Submit" class="btn btn-success btn-lg btn-block">
 
